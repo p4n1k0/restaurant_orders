a10e1d38cd2c3fcfca8747ad45e0e882c1893fc1
p4n1k0 <gabre.tarick@gmail.com>
2023-05-16 11:17:32 -0300

    Testes p classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient('carne')
+    ingredient_2 = Ingredient('farinha')
+    assert ingredient_1.name == 'carne'
+    assert ingredient_1 == ingredient_1
+    assert ingredient_1 != ingredient_2
+    assert hash(ingredient_1) == hash('carne')
+    assert repr(ingredient_1) == "Ingredient('carne')"
+    assert ingredient_1.restrictions != ingredient_2.restrictions
