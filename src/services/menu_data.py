e5ae3b00d785297317a27d0392653d5811d88ab2
p4n1k0 <gabre.tarick@gmail.com>
2025-01-08 19:10:02 -0300

    teste

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,25 +1,35 @@
 from src.models.dish import Dish
 from src.models.ingredient import Ingredient
-from typing import Dict
 import csv
 
-Dishes = Dict[Dish, Dish]
 
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        dishes_dict: Dishes = {}
-        with open(source_path) as file:
-            rows = csv.DictReader(file)
-            for row in rows:
-                dish_name = row['dish']
-                if dish_name not in dishes_dict:
-                    dishes_dict[dish_name] = Dish(
-                        dish_name,
-                        float(row['price'])
-                    )
-                dishes_dict[dish_name].add_ingredient_dependency(
-                    Ingredient(row['ingredient']),
-                    int(row['recipe_amount'])
+        self.dishes = set()
+        self.ingredients = set()
+        self.load_menu_data(source_path)
+
+    def load_menu(self, source_path: str) -> None:
+        with open(source_path, newline='') as file:
+            reader = csv.reader(file)
+            next(reader)
+            dishes = {}
+            for row in reader:
+                (
+                    dish_name,
+                    dish_price,
+                    ingredient_name,
+                    ingredient_quantity
+                ) = row
+                dish = dishes.get(dish_name)
+                if not dish:
+                    dish = Dish(dish_name, float(dish_price))
+                    dishes[dish_name] = dish
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(
+                    ingredient,
+                    int(ingredient_quantity)
                 )
-        self.dishes = set(dishes_dict.values())
+                self.ingredients.add(ingredient)
+        self.dishes.update(dishes.values())
