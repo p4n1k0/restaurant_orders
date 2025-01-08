773ef89769c739bf1a2efe9b06a13de1f98e481e
p4n1k0 <gabre.tarick@gmail.com>
2025-01-08 20:23:41 -0300

    refatora teste classe ingrediente

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,5 +1,5 @@
-from src.models.dish import Dish
-from src.models.ingredient import Ingredient
+from models.dish import Dish
+from models.ingredient import Ingredient
 import csv
 
 
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -5,9 +5,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 def test_ingredient():
     ingredient_1 = Ingredient('carne')
     ingredient_2 = Ingredient('farinha')
+    
+    assert ingredient_1 is ingredient_1
+    assert ingredient_1 != ingredient_2
+    assert hash(ingredient_1) == hash(ingredient_1.name)
+    assert str(ingredient_1) == f"Ingredient('{ingredient_1.name}')"
+    assert ingredient_1.restrictions != set()
     assert ingredient_1.name == 'carne'
     assert ingredient_1 == ingredient_1
     assert ingredient_1 != ingredient_2
-    assert hash(ingredient_1) == hash('carne')
-    assert repr(ingredient_1) == "Ingredient('carne')"
-    assert ingredient_1.restrictions != ingredient_2.restrictions
